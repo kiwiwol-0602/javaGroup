1c30e59ef7d7ba73f02a407a5740422e9b96f3eb
kiwiwol-0602 <soso1518@naver.com>
2024-09-10 18:20:44 +0900

    20240910 1820 숙제 말말말

--- src/t50_Exercise/T6.java
+++ src/t50_Exercise/T6.java
@@ -1,4 +1,7 @@
 package t50_Exercise;
+
+import java.util.Scanner;
+
 /*숙제
  1. 말 5마리
  2. 말선택을 하면
@@ -7,7 +10,30 @@ package t50_Exercise;
  5. 결승점을 50이나 100으로 두고
  6. 선이 움직이게끔
  7. 1번말 200, 2번말 150, 3번말 100, 4번말 0 5번말 0
+ 8. 배팅 획득금액까지
  */
 public class T6 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("얼마를 배팅하시겠습니까? ");
+		int money = sc.nextInt();
+		
+		System.out.println("배팅할 말을 골라주세요(숫자로 선택) ");
+		System.out.println("1.연말|2.월말|3.주말|4.정말|5.옳은말");
+		int choice = sc.nextInt();
+		
+		//동기화 .join()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
 }
