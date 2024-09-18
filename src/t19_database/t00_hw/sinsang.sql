aab12d3fe73e20c344c793f78735728ff938d4d9
kiwiwol-0602 <soso1518@naver.com>
2024-09-18 13:03:01 +0900

    20240918_1302  과제

--- src/t19_database/t00_hw/sinsang.sql
+++ src/t19_database/t00_hw/sinsang.sql
@@ -44,6 +44,62 @@ desc sinsang;
 
 select * from sinsang;
 
-/*update sinsang set point= point + 100 where gender = '여자';*/
+update sinsang set point = point + 100 where gender = '여자';
 
-/*update sinsang set address='서울' where mid = 'kim1';*/
+select * from sinsang;
+
+update sinsang set point = point + 50 where gender = '남자' and age>=35;
+
+select * from sinsang;
+
+update sinsang set address='서울' where mid = 'kim1';
+
+select mid, age, address from sinsang;
+
+select  mid as 아이디, gender as 성별, ipsail as 입사일 from sinsang where ipsail >= '2000-01-01';
+
+insert into sinsang values ('park','1234','박세리',35,'여자',default,'인천',default);
+
+select mid,gender, address, age from sinsang where (age>=30 and age<40) and gender='여자';
+
+update sinsang set point = point + 500 where gender = '여자' and address !='서울' and address != '인천' ;
+
+update sinsang set point = 0 where gender = '남자' and ipsail<'1990-01-01' ;
+
+update sinsang set gender = '여자' where point = 0 and age >= 40 ;
+
+update sinsang set pwd = '0000' and point = point + 300 where address = '서울';
+
+delete from sinsang where point=0 and gender = '남자' and ipsail<'1985-01-01';
+
+select * from sinsang where gender='여자' and age >=30 and age<40; 
+
+select * from sinsang where gender='여자' and age between 30 and 39;
+
+select name, age, gender, address from sinsang where address= '서울' or address= '부산' ;
+
+select name, age, gender, address from sinsang where address in ('서울','부산');
+
+select * from sinsang order by age desc, name,  idx ;
+
+insert into sinsang (name, mid, pwd) values ('홍길공자','hkgj','1234');
+
+select * from sinsang where name like '홍%';
+
+select * from sinsang where name like '%자';
+
+select * from sinsang where name like '%공%';
+
+select mid as '아이디', name as '이름', gender as '성별' from sinsang where  name like '_기%';
+
+select * from sinsang where name like '%공_';
+
+select mid, name, gender from sinsang order by idx limit 5; 
+
+select mid as '아이디', name as '이름', gender as '성별' from sinsang order by idx limit 4,5;
+
+select mid, name, gender from sinsang order by gender desc, name, idx limit 5; 
+
+select * from sinsang order by age desc limit 3;
+
+  
